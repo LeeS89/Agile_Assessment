d56cf7e2a9c1c774eb91e65979ecb8ecff8a0807
LeeS89 <leeslammon@outlook.com>
2022-03-24 10:38:40 +0000

    Version 2

--- CommissionCalculator/src/GrossPay.java
+++ CommissionCalculator/src/GrossPay.java
@@ -2,6 +2,12 @@ public class GrossPay {
 
     public double TotalGrossPay(double grossSales) throws InvalidValueException
     {
-        throw new InvalidValueException("No code written yet");
+        double commission = grossSales * 0.09;
+        double totalPay = grossSales + commission + 200.00;
+
+        if(grossSales < 100.00 || grossSales > 100000.00) {
+            throw new InvalidValueException("Invalid grossSales entered");
+        }
+        return totalPay;
     }
 }
--- CommissionCalculator/src/GrossPayTest.java
+++ CommissionCalculator/src/GrossPayTest.java
@@ -19,7 +19,7 @@ public class GrossPayTest extends TestCase {
 
     }
 
-    //Test number 2:
+    //Test number: 2
     //Objective: Invalid low grossSales
     //Input(s): grossSales = 50.00
     //Expected output: "Invalid grossSales entered"
@@ -37,9 +37,9 @@ public class GrossPayTest extends TestCase {
         }
     }
 
-    //Test number 3:
+    //Test number: 3
     //Objective: Invalid high grossSales
-    //Input(s): 150,000
+    //Input(s): grossSales = 150,000
     //Expected output: "Invalid grossSales entered"
     public void testTotalGrossPay003()
     {
@@ -55,7 +55,75 @@ public class GrossPayTest extends TestCase {
         }
     }
 
+    //Test number: 4
+    //Objective: Valid low boundary grossSales
+    //Input(s); grossSales = 100.00
+    //Expected output: 309.00
+    public void testTotalGrossPay004()
+    {
+        GrossPay grossPay = new GrossPay();
 
+        try{
+            assertEquals(309.00, grossPay.TotalGrossPay(100.00));
 
+        }catch(InvalidValueException e)
+        {
+            fail("No exception expected");
+        }
+    }
+
+    //Test number: 5
+    //Objective: Valid high boundary grossSales
+    //Input(s): grossSales = 100,000.00
+    //Expected output: 109,200.00
+    public void testTotalGrossPay005()
+    {
+        GrossPay grossPay = new GrossPay();
+
+        try{
+            assertEquals(109200.00,grossPay.TotalGrossPay(100000.00));
+        }catch(InvalidValueException e)
+        {
+            fail("No exception expected");
+        }
+    }
+
+    //Test number: 6
+    //Objective: Invalid low boundary grossSales
+    //Input(s): grossSales = 99.99
+    //Expected output: "Invalid grossSales entered"
+    public void testTotalGrossPay006()
+    {
+        GrossPay grossPay = new GrossPay();
+        double actualPay = 0.0;
+
+        try{
+            actualPay = grossPay.TotalGrossPay(99.99);
+            fail("Exception expected");
+
+        }catch(InvalidValueException e)
+        {
+            assertEquals("Invalid grossSales entered",e.getMessage());
+        }
+    }
+
+    //Test number: 7
+    //Objective: Invalid high boundary grossSales
+    //Input(s): grossSales = 100,000.01
+    //Expected output: "Invalid grossSales entered"
+    public void testTotalGrossPay007()
+    {
+        GrossPay grossPay = new GrossPay();
+        double actualPay = 0.0;
+
+        try{
+            actualPay = grossPay.TotalGrossPay(100000.01);
+            fail("Exception expected");
+
+        }catch(InvalidValueException e)
+        {
+            assertEquals("Invalid grossSales entered",e.getMessage());
+        }
+    }
 
 }
